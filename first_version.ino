264f2b993dc2e1f2e53063f1cf4d357f373f6739
powerbash6a2 <mehradmohamadi1392@gmail.com>
2024-11-12 20:05:56 +0330

    first_version

---
+++ first_version.ino
@@ -0,0 +1,12 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("hello world");
+}
+
+void loop() {
+}
