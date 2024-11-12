382b750bf07477e42af4c122ce4ca4630837456d
powerbash6a2 <mehradmohamadi1392@gmail.com>
2024-11-12 20:59:50 +0330

    second_version

--- first_version.ino
+++ first_version.ino
@@ -1,12 +1,17 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.clear();
-  lcd.print("hello world");
+  Serial.print("enter a number");
 }
 
 void loop() {
+  int a = Serial.parseInt();
+ if(Serial.available() > 0){
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print(a);
+  }
 }
